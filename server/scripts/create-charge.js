3507034029a6a6bcb0b25dccfb225cda5f0035d3
Felix Tellmann <felix@tellmann.co.za>
2017-12-15 21:10:41 +0200

    Application Charges - Once off first approach done

--- server/scripts/create-charge.js
+++ server/scripts/create-charge.js
@@ -54,8 +54,6 @@ const createCharge = async (req, res, next) => {
     }
 
     /*================ If no Charge is setup, activate free charge to fast forward the chargeCheck ================*/
-    console.log(application_charge);
-    console.log(recurring_application_charge);
     if (application_charge <= 0 && recurring_application_charge <= 0) {
         options = false;
     }
