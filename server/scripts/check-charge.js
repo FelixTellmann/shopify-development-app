96496c8a1e420070340aab7a1cb1e855ac3197c2
Felix Tellmann <felix@tellmann.co.za>
2017-12-15 21:02:42 +0200

    Application Charges - Once off first approach done

--- server/scripts/check-charge.js
+++ server/scripts/check-charge.js
@@ -3,8 +3,8 @@ import Shop from '../models/shop';
 const chargeRenew = process.env.SHOPIFY_APP_CHARGE_RENEW == 'true' || false;
 
 const checkCharge = async (req, res, next) => {
-    const {charge_approved} = await Shop.findOne({shop_URI: req.user.shop_URI});
-    if (charge_approved && !chargeRenew) {
+    const shop = await Shop.findOne({shop_URI: req.user.shop_URI});
+    if (shop.charge_approved && !chargeRenew) {
         res.redirect('/app')
     } else {
         next();
