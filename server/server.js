9aa63915b053c17e0a2e539eb93f0efe09cbae3f
Felix Tellmann <felix@tellmann.co.za>
2018-04-29 12:29:08 +0200

    Application Charges - Once off first approach done

--- server/server.js
+++ server/server.js
@@ -32,8 +32,9 @@ app.use(express.urlencoded({extended: false}));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
